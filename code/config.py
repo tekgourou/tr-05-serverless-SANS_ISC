4b86abb3bf350c7b5999456bfc8e4685d16b0242
Alex <aargeris@cisco.com>
2021-10-04 14:35:56 -0400

    v1

---
+++ code/config.py
@@ -0,0 +1,21 @@
+import os
+
+from __version__ import VERSION
+
+
+class Config:
+    VERSION = VERSION
+
+    #SECRET_KEY = os.environ.get('SECRET_KEY', None)
+    SECRET_KEY = 'not use'
+
+
+    # Supported types with rules
+    CCT_OBSERVABLE_TYPES = {
+        'ip': {}
+    }
+
+    CTR_HEADERS = {
+        'User-Agent': ('SecureX Threat Response Integrations '
+                       '<tr-integrations-support@cisco.com>')
+    }
